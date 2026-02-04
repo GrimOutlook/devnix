82158ad654e2acea3bc66397e8aa1928f1235f50
Dominic Grimaldi <dev@grimoutlook.dev>
2026-02-03 18:30:00 -0600

    feat: Added desktop key to agenix keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   home_laptop_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKioB5C0OpoFNFHKEaKFGcaI50070Q/NvYUM0SMgxVP";
-  keys = [home_laptop_key];
+  home_desktop_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZNqwWqgoIvHXT5bWO5wTvaeXEK40Ozc3dv53TON3+J";
+  keys = [home_laptop_key home_desktop_key];
 in {
   "github.com.age".publicKeys = keys;
 }
