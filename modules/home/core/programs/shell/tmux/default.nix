c801f312c00c9bc9a951c68582b166700c5d2f06
Dominic Grimaldi <dev@grimoutlook.dev>
2026-02-11 09:28:49 -0600

    Update TMUX

--- modules/home/core/programs/shell/tmux/default.nix
+++ modules/home/core/programs/shell/tmux/default.nix
@@ -4,25 +4,35 @@
     {
       programs.tmux = {
         enable = true;
-
+        # Use 24 hour clock.
+        clock24 = true;
+        # Maximum number of lines held in window history.
+        historyLimit = 10000;
+        # Automatically spawn a session if trying to attach and none are running.
+        # WARN: Don't turn this on. This appears to already be default behavior and when this is
+        # enabled it ends up spawning 2 sessions.
+        newSession = false;
+        # Set the $TERM variable. Use tmux-direct if italics or 24bit true color support is needed.
+        terminal = "tmux-direct";
         # Rather than constraining window size to the maximum size of any client
         # connected to the *session*, constrain window size to the maximum size of any
         # client connected to *that window*. Much more reasonable.
         aggressiveResize = true;
-
-        clock24 = true;
-
         # Allows for faster key repetition
         escapeTime = 50;
-
-        keyMode = "vi";
-        # Overrides the hjkl and HJKL bindings for pane navigation and resizing in VI mode
-        customPaneNavigationAndResize = true;
-
+        # On supported terminals, request focus events and pass them through to
+        # applications running in tmux.
+        focusEvents = true;
+        # List of plugins to install.
         plugins = with pkgs.tmuxPlugins; [
           gruvbox
+          sensible
+          tmux-powerline
+          tmux-which-key
+          vim-tmux-navigator
+          yank
         ];
-
+        # Additional contents of /etc/tmux.conf, to be run after sourcing plugins.
         extraConfig = builtins.readFile ./tmux.conf;
       };
 
@@ -33,5 +43,22 @@
         tma = "tmux attach -t";
         tmk = "tmux kill-session -t";
       };
+
+      
+      programs.bash.initExtra = builtins.readFile ./config.sh + builtins.readFile ./theme.sh + ''
+        # Verify that:
+        # 1. TMUX command exists
+        # 2. PS1 string is set and isn't ""
+        # 3. We aren't in a screen session
+        # 4. We aren't in a TMUX session already
+        # 5. Additional check to make sure we aren't in a tmux session
+        if command -v tmux &>/dev/null \
+          && [ -n "$PS1" ] \
+          && [[ ! "$TERM" =~ screen ]] \
+          && [[ ! "$TERM" =~ tmux ]] \
+          && [ -z "$TMUX" ]; then
+          tmux new-session
+        fi
+      '';
     };
 }
