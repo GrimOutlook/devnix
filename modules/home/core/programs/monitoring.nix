c8e5e587c60ed99d9a21604dfac737cd2ef83c07
Dominic Grimaldi <dev@grimoutlook.dev>
2026-02-11 20:49:10 -0600

    feat: Add sysz

--- modules/home/core/programs/monitoring.nix
+++ modules/home/core/programs/monitoring.nix
@@ -1,21 +1,26 @@
 {
-  flake.modules.homeManager.core = {
-    programs = {
-      bottom.enable = true;
-      htop.enable = true;
+  flake.modules.homeManager.core = 
+    {pkgs, ...}:
+    {
+      home.packages = with pkgs; [
+        sysz
+      ];
+      programs = {
+        bottom.enable = true;
+        htop.enable = true;
 
-      btop = {
-        enable = true;
+        btop = {
+          enable = true;
 
-        settings = {
-          color_theme = "gruvbox_dark";
-          vim_keys = true;
-          update_ms = 1000;
-          disks_filter = "";
-          mem_graphs = false;
-          proc_per_core = true;
+          settings = {
+            color_theme = "gruvbox_dark";
+            vim_keys = true;
+            update_ms = 1000;
+            disks_filter = "";
+            mem_graphs = false;
+            proc_per_core = true;
+          };
         };
       };
     };
-  };
 }
