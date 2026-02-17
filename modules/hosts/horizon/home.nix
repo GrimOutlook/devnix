86a72c77ac9e1c848050017a5bba182f10649339
Dominic Grimaldi <dev@grimoutlook.dev>
2026-02-17 17:37:09 -0600

    feat: Create ssh-config entries

--- modules/hosts/horizon/home.nix
+++ modules/hosts/horizon/home.nix
@@ -1,10 +1,27 @@
 { config, ... }:
+let
+  keys = "~/.ssh/keys";
+in
 {
   homeHosts.horizon = {
     unstable = true;
     modules = with config.flake.modules.homeManager; [
       dev
       ssh-github
+      {
+        programs.ssh.matchBlocks = {
+          Brussels = {
+            hostname = "brussels.home.arpa";
+            user = "root";
+            identityFile = "${keys}/brussels";
+          };
+          Washington = {
+            hostname = "washington.home.arpa";
+            user = "root";
+            identityFile = "${keys}/washington";
+          };
+        };
+      }
     ];
   };
 }
